5965d0af196d0e3b9919e3b6c4532da620914c04
krishpranav <krisna.pranav@gmail.com>
2021-11-14 11:48:38 +0530

    QRReaderView: update widgets + core settings

--- QRReader/QRReaderView.swift
+++ QRReader/QRReaderView.swift
@@ -11,7 +11,7 @@ import AVFoundation
 public protocol QRReaderViewDelegate: AnyObject {
     func qrReaderView(_ qrReaderView: QRReaderView, didFailure error: QRReaderErrors)
     func qrReaderView(_ qrReaderView: QRReaderView, didSuccess code: String)
-    
+
     func qrReaderView(_ qrReaderView: QRReaderView, didChangeTorchActive isOn: Bool)
 }
 
@@ -21,40 +21,39 @@ public extension QRReaderViewDelegate {
 
 @IBDesignable
 public class QRReaderView: UIView {
+
     public struct Input {
         let focusImage: UIImage?
         let focusImagePadding: CGFloat?
-        let animationDuraction: Double?
+        let animationDuration: Double?
         let isBlurEffectEnabled: Bool?
-        
+
         public static var `default`: Input {
             return .init(focusImage: nil, focusImagePadding: nil, animationDuration: nil, isBlurEffectEnabled: nil)
-
         }
-        
-        public init(focusImage: UIImage? = nil, focusImagePadding: CGFloat? = nil, animationDuration: Double? = nil, isBlurEffectEnabled: Bool? = nil)  {
+
+        public init(focusImage: UIImage? = nil, focusImagePadding: CGFloat? = nil, animationDuration: Double? = nil, isBlurEffectEnabled: Bool? = nil) {
             self.focusImage = focusImage
             self.focusImagePadding = focusImagePadding
-            self.animationDuraction = animationDuration
+            self.animationDuration = animationDuration
             self.isBlurEffectEnabled = isBlurEffectEnabled
         }
     }
-    
+
     @IBInspectable
     public var focusImage: UIImage?
-    
+
     @IBInspectable
     public var focusImagePadding: CGFloat = 8.0
-    
+
     @IBInspectable
-    public var animationDuraction: Double = 0.5
-    
+    public var animationDuration: Double = 0.5
+
     @IBInspectable
     public var isBlurEffectEnabled: Bool = false
-    
+
     public func configure(delegate: QRReaderViewDelegate, input: Input = .default) {
         self.delegate = delegate
-        
         if let focusImage = input.focusImage {
             self.focusImage = focusImage
         }
@@ -73,36 +72,64 @@ public class QRReaderView: UIView {
         setupBlurEffectView()
         setupImageViews()
     }
-    
+
     public func startRunning() {
         guard isAuthorized() else { return }
-        guard !session?.isRunning else { return }
+        guard !session.isRunning else { return }
         videoDataOutputEnable = false
         metadataOutputEnable = true
         metadataQueue.async { [weak self] in
             self?.session.startRunning()
         }
     }
-    
-    
-    /* main funcs */
-    private func configureSession() {
-        
+
+    public func stopRunning() {
+        guard session.isRunning else { return }
+        videoDataQueue.async { [weak self] in
+            self?.session.stopRunning()
+        }
+        metadataOutputEnable = false
+        videoDataOutputEnable = false
     }
-    
-    private func setupBlurEffectView() {
-        
+
+    public func rescan() {
+        guard isAuthorized() else { return }
+        if isBlurEffectEnabled {
+            blurEffectView.isHidden = true
+        }
+        focusImageView.removeFromSuperview()
+        qrCodeImageView.removeFromSuperview()
+        setupImageViews()
+        qrCodeImage = nil
+        videoDataOutputEnable = false
+        metadataOutputEnable = true
     }
-    
-    private func setupImageViews() {
+
+    public func setTorchActive(isOn: Bool) {
+        assert(Thread.isMainThread)
         
+        guard let videoDevice = AVCaptureDevice.default(for: .video),
+            videoDevice.hasTorch, videoDevice.isTorchAvailable,
+            (metadataOutputEnable || videoDataOutputEnable) else {
+                return
+        }
+        try? videoDevice.lockForConfiguration()
+        videoDevice.torchMode = isOn ? .on : .off
+        videoDevice.unlockForConfiguration()
     }
-    
-    private func addPreviewLayer() {
-        
+
+    deinit {
+        setTorchActive(isOn: false)
+        focusImageView.removeFromSuperview()
+        qrCodeImageView.removeFromSuperview()
+        session.inputs.forEach { session.removeInput($0) }
+        session.outputs.forEach { session.removeOutput($0) }
+        removePreviewLayer()
+        torchActiveObservation = nil
     }
-    
+
     // MARK: - Private Functions
+
     private weak var delegate: QRReaderViewDelegate?
     private let metadataQueue = DispatchQueue(label: "metadata.session.qrreader.queue")
     private let videoDataQueue = DispatchQueue(label: "videoData.session.qrreader.queue")
@@ -126,11 +153,11 @@ public class QRReaderView: UIView {
     private enum AuthorizationStatus {
         case authorized, notDetermined, restrictedOrDenied
     }
-    
+
     private func isAuthorized() -> Bool {
-        return authorizationStatus == .authorized
+        return authorizationStatus() == .authorized
     }
-    
+
     private func authorizationStatus() -> AuthorizationStatus {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
         case .authorized:
@@ -147,5 +174,229 @@ public class QRReaderView: UIView {
         @unknown default:
             return .restrictedOrDenied
         }
+    }
+
+    // MARK: Configuration
+    private func configureSession() {
+        guard let videoDevice = AVCaptureDevice.default(for: .video) else {
+            failure(.deviceFailure(.videoUnavailable))
+            return
+        }
+
+        guard let videoInput = try? AVCaptureDeviceInput(device: videoDevice), session.canAddInput(videoInput) else {
+            failure(.deviceFailure(.inputInvalid))
+            return
+        }
+
+        guard session.canAddOutput(metadataOutput) else {
+            failure(.deviceFailure(.metadataOutputFailure))
+            return
+        }
+
+        guard session.canAddOutput(videoDataOutput) else {
+            failure(.deviceFailure(.videoDataOutputFailure))
+            return
+        }
+
+        session.beginConfiguration()
+        session.addInput(videoInput)
+        metadataOutput.setMetadataObjectsDelegate(self, queue: metadataQueue)
+        session.addOutput(metadataOutput)
+        metadataOutput.metadataObjectTypes = [.qr]
+
+        videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+        videoDataOutput.setSampleBufferDelegate(self, queue: videoDataQueue)
+        session.addOutput(videoDataOutput)
+
+        session.commitConfiguration()
+
+        if videoDevice.hasTorch {
+            torchActiveObservation = videoDevice.observe(\.isTorchActive, options: .new) { [weak self] _, change in
+                self?.didChangeTorchActive(isOn: change.newValue ?? false)
+            }
+        }
+
+        if authorizationStatus() == .notDetermined {
+            videoDataOutputEnable = false
+            metadataOutputEnable = true
+            metadataQueue.async { [weak self] in
+                self?.session.startRunning()
+            }
+        }
+    }
+
+    private func setupBlurEffectView() {
+        guard isBlurEffectEnabled else { return }
+        blurEffectView.isHidden = true
+        addSubview(blurEffectView)
+    }
+
+    private func setupImageViews() {
+        let width = self.bounds.width * 0.618
+        let x = self.bounds.width * 0.191
+        let y = self.bounds.height * 0.191
+        focusImageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: width))
+        focusImageView.image = focusImage ?? UIImage(named: "scan_qr_focus", in: .module, compatibleWith: nil)
+        addSubview(focusImageView)
+
+        qrCodeImageView = UIImageView()
+        qrCodeImageView.contentMode = .scaleAspectFill
+        addSubview(qrCodeImageView)
+    }
+
+    private func addPreviewLayer() {
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = self.bounds
+        layer.addSublayer(previewLayer)
+
+        self.previewLayer = previewLayer
+    }
+
+    private func removePreviewLayer() {
+        previewLayer?.removeFromSuperlayer()
+    }
+
+    private func moveImageViews(qrCode: String, corners: [CGPoint]) {
+        assert(Thread.isMainThread)
+        
+        let path = UIBezierPath()
+        path.move(to: corners[0])
+        corners[1..<corners.count].forEach() {
+            path.addLine(to: $0)
+        }
+        path.close()
+
+        let aSide: CGFloat
+        let bSide: CGFloat
+        if corners[0].x < corners[1].x {
+            aSide = corners[0].x - corners[1].x
+            bSide = corners[1].y - corners[0].y
+        } else {
+            aSide = corners[2].y - corners[1].y
+            bSide = corners[2].x - corners[1].x
+        }
+        let degrees = atan(aSide / bSide)
+
+        var maxSide: CGFloat =  hypot(corners[3].x - corners[0].x, corners[3].y - corners[0].y)
+        for (i, _) in corners.enumerated() {
+            if i == 3 { break }
+            let side = hypot(corners[i].x - corners[i+1].x, corners[i].y - corners[i+1].y)
+            maxSide = side > maxSide ? side : maxSide
+        }
+        maxSide += focusImagePadding * 2
+
+        UIView.animate(withDuration: animationDuration, animations: { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.focusImageView.frame = path.bounds
+            let center = strongSelf.focusImageView.center
+            strongSelf.focusImageView.frame.size = CGSize(width: maxSide, height: maxSide)
+            strongSelf.focusImageView.center = center
+            strongSelf.focusImageView.transform = CGAffineTransform.identity.rotated(by: degrees)
+
+            strongSelf.qrCodeImageView.frame = path.bounds
+            strongSelf.qrCodeImageView.center = center
+            }, completion: { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.qrCodeImageView.image = strongSelf.qrCodeImage
+                if strongSelf.isBlurEffectEnabled {
+                    strongSelf.blurEffectView.isHidden = false
+                }
+                strongSelf.success(qrCode)
+        })
+    }
+
+    private func failure(_ error: QRReaderErrors) {
+        delegate?.qrReaderView(self, didFailure: error)
+    }
+
+    private func success(_ code: String) {
+        delegate?.qrReaderView(self, didSuccess: code)
+    }
+
+    private func didChangeTorchActive(isOn: Bool) {
+        delegate?.qrReaderView(self, didChangeTorchActive: isOn)
+    }
+}
+
+extension QRReaderView: AVCaptureMetadataOutputObjectsDelegate {
+    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard metadataOutputEnable else { return }
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = previewLayer?.transformedMetadataObject(for: metadataObject) as? AVMetadataMachineReadableCodeObject, metadataObject.type == .qr else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            metadataOutputEnable = false
+            videoDataOutputEnable = true
+
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                strongSelf.setTorchActive(isOn: false)
+                strongSelf.moveImageViews(qrCode: stringValue, corners: readableObject.corners)
+            }
+        }
+    }
+}
+
+extension QRReaderView: AVCaptureVideoDataOutputSampleBufferDelegate {
+    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        connection.videoOrientation = .portrait
+        guard videoDataOutputEnable else { return }
+        guard let qrCodeImage = getImageFromSampleBuffer(sampleBuffer: sampleBuffer) else { return }
+
+        self.qrCodeImage = qrCodeImage
+        videoDataOutputEnable = false
+    }
+
+    private func getImageFromSampleBuffer(sampleBuffer: CMSampleBuffer) -> UIImage? {
+        let scale = UIScreen.main.scale
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
+        CVPixelBufferLockBaseAddress(pixelBuffer, .readOnly)
+        let baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer)
+        let width = CVPixelBufferGetWidth(pixelBuffer)
+        let height = CVPixelBufferGetHeight(pixelBuffer)
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue)
+        guard let context = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue) else { return nil }
+        guard let cgImage = context.makeImage() else { return nil }
+
+        let sampleBuffer = UIImage(cgImage: cgImage, scale: scale, orientation: .up)
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, .readOnly)
+
+        return readQRCode(sampleBuffer)
+    }
+
+    private func readQRCode(_ image: UIImage) -> UIImage? {
+        guard let ciImage = CIImage(image: image) else { return nil }
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
+        guard let features = detector?.features(in: ciImage) else { return nil }
+        guard let feature = features.first as? CIQRCodeFeature else { return nil }
+
+        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -ciImage.extent.size.height)
+        let path = UIBezierPath()
+        path.move(to: feature.topLeft.applying(transform))
+        path.addLine(to: feature.topRight.applying(transform))
+        path.addLine(to: feature.bottomRight.applying(transform))
+        path.addLine(to: feature.bottomLeft.applying(transform))
+        path.close()
+        return image.crop(path)
+    }
+}
+
+private extension UIImage {
+    func crop(_ path: UIBezierPath) -> UIImage? {
+        let rect = CGRect(origin: CGPoint(), size: CGSize(width: size.width * scale, height: size.height * scale))
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, scale)
+
+        UIColor.clear.setFill()
+        UIRectFill(rect)
+        path.addClip()
+        draw(in: rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        guard let croppedImage = image?.cgImage?.cropping(to: CGRect(x: path.bounds.origin.x * scale, y: path.bounds.origin.y * scale, width: path.bounds.size.width * scale, height: path.bounds.size.height * scale)) else { return nil }
+        return UIImage(cgImage: croppedImage, scale: scale, orientation: imageOrientation)
     }
 }
