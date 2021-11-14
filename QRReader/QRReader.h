a45f4352c25be488dd547966ee4f017d99440be6
krishpranav <krisna.pranav@gmail.com>
2021-11-14 10:56:27 +0530

    Add files via upload

---
+++ QRReader/QRReader.h
@@ -0,0 +1,13 @@
+//
+//  QRReader.h
+//  QRReader
+//
+//  Created by Apple on 14/11/21.
+//
+
+/* imports */
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double QRReaderVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char QRReaderVersionString[];
