3fec6b96638fba48d24a7ba5869ce700619f149a
krishpranav <krisna.pranav@gmail.com>
2021-11-14 11:16:59 +0530

    QRReader: QRReader Errors

---
+++ QRReader/QRReaderErrors.swift
@@ -0,0 +1,23 @@
+//
+//  QRReaderErrors.swift
+//  QRReader
+//
+//  Created by Apple on 14/11/21.
+//
+
+import Foundation
+import AVFoundation
+
+public enum QRReaderErrors: Error {
+    case unauthorized(AVAuthorizationStatus)
+    case deviceFailure(DeviceError)
+    case readFailure
+    case unknown
+    
+    public enum DeviceError {
+        case videoUnavailable
+        case inputInvalid
+        case metadataOutputFailure
+        case videoDataOutputFailure
+    }
+}
