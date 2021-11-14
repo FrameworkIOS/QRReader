c934df8936873f4a4d326f81fb40254efe321d6a
krishpranav <krisna.pranav@gmail.com>
2021-11-14 13:44:13 +0530

    SampleApp: FlashButton

--- SampleApp/SampleApp/FlashButton.swift
+++ SampleApp/SampleApp/FlashButton.swift
@@ -5,32 +5,40 @@
 //  Created by Apple on 14/11/21.
 //
 
+/* imports */
 import UIKit
 
+/* flash button */
 final class FlashButton: UIButton {
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         settings()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         settings()
     }
-    
+
     override var isSelected: Bool {
         didSet {
             let color: UIColor = isSelected ? .gray : .lightGray
             backgroundColor = color.withAlphaComponent(0.7)
         }
     }
-    
 }
 
 private extension FlashButton {
+    /* settings */
     func settings() {
         setTitleColor(.darkGray, for: .normal)
         setTitleColor(.white, for: .selected)
+        setTitle("OFF", for: .normal)
+        setTitle("ON", for: .selected)
+        tintColor = .clear
+        titleLabel?.font = .boldSystemFont(ofSize: 16)
+        layer.cornerRadius = frame.size.width / 2
         isSelected = false
     }
 }
